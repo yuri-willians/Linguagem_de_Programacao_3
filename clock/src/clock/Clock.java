ca8f4bbd27d59f78b81b6f53f8a7ed60ebb080be
Yuri WIllians dos Santos <42418401+ShieldOfDead@users.noreply.github.com>
2020-02-19 11:09:12 -0400

    Visual update

--- clock/src/clock/Clock.java
+++ clock/src/clock/Clock.java
@@ -5,7 +5,10 @@
  */
 package clock;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -14,22 +17,26 @@ import javafx.stage.Stage;
  *
  * @author Yuri
  */
-public class Clock extends Application {
+public class Clock extends Application implements Runnable {
 
     /**
      * @param args the command line arguments
      */
     Stage stage = new Stage();
+    Pane pane = new Pane();
+    Scene scene = new Scene(pane, 500, 500);
 
     @Override
     public void start(Stage stage) {
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, 500, 500);
 
         Circle.makeCircle(stage, scene, pane);
         Pointers.makePointers(stage, scene, pane);
         Texts.makeTexts(stage, scene, pane);
         CenterCircle.makeCenterCircle(stage, scene, pane);
+
+        Thread t = new Thread(this);
+        t.setDaemon(true);
+        t.start();
 
         stage.setScene(scene);
         stage.show();
@@ -37,6 +44,35 @@ public class Clock extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.getMessage();
+            }
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    pane.getChildren().clear();
+
+                    Circle.makeCircle(stage, scene, pane);
+
+                    Pointers.makePointers(stage, scene, pane);
+
+                    Texts.makeTexts(stage, scene, pane);
+
+                    CenterCircle.makeCenterCircle(stage, scene, pane);
+                }
+
+            });
+
+        }
+
     }
 
 }
--- clock/src/clock/Pointers.java
+++ clock/src/clock/Pointers.java
@@ -5,10 +5,13 @@
  */
 package clock;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 /**
@@ -18,27 +21,39 @@ import javafx.stage.Stage;
 public class Pointers {
 
     static public void makePointers(Stage stage, Scene scene, Pane pane) {
+
+        Calendar calendar = new GregorianCalendar();
+
+        int hourInt, minuteInt, secondInt;
+
+        hourInt = calendar.get(Calendar.HOUR_OF_DAY);
+        minuteInt = calendar.get(Calendar.MINUTE);
+        secondInt = calendar.get(Calendar.SECOND);
+
         Line hour = new Line();
         hour.setStartX(pane.getWidth() / 2);
         hour.setStartY(pane.getHeight() / 2);
-        hour.setEndX(pane.getWidth() / 2 + 120);
-        hour.setEndY(pane.getHeight() / 2 + 30);
+        hour.setEndX(pane.getWidth() / 2);
+        hour.setEndY(pane.getHeight() / 2 - 80);
+        hour.getTransforms().add(new Rotate((hourInt * 30 + (minuteInt / 60) * 30), pane.getWidth() / 2, pane.getWidth() / 2));
         hour.setStroke(Color.RED);
         pane.getChildren().add(hour);
 
         Line minute = new Line();
         minute.setStartX(pane.getWidth() / 2);
         minute.setStartY(pane.getHeight() / 2);
-        minute.setEndX(pane.getWidth() / 2 + 120);
-        minute.setEndY(pane.getHeight() / 2 + 60);
+        minute.setEndX(pane.getWidth() / 2);
+        minute.setEndY(pane.getHeight() / 2 - 120);
+        minute.getTransforms().add(new Rotate(minuteInt * 6, pane.getWidth() / 2, pane.getWidth() / 2));
         minute.setStroke(Color.BLUE);
         pane.getChildren().add(minute);
 
         Line second = new Line();
         second.setStartX(pane.getWidth() / 2);
         second.setStartY(pane.getHeight() / 2);
-        second.setEndX(pane.getWidth() / 2 + 120);
-        second.setEndY(pane.getHeight() / 2 + 120);
+        second.setEndX(pane.getWidth() / 2);
+        second.setEndY(pane.getHeight() / 2 - 180);
+        second.getTransforms().add(new Rotate(secondInt * 6, pane.getWidth() / 2, pane.getWidth() / 2));
         second.setStroke(Color.GREEN);
         pane.getChildren().add(second);
 
